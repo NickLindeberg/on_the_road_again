9a83d5c2a2db7f75a983886cc2baabbf2fe09e31
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-12 16:00:22 -0700

    setup for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: "welcome#index"
   get '/about', to: "about#index"
+  get '/dashboard', to: "artist#index"
 
+  resources :venues, only: [:show]
+  resources :events, only: [:show, :new, :create, :edit, :update]
+  resources :tours, only: [:show, :new, :create, :edit, :update]
 end
