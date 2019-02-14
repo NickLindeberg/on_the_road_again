64c688b7c81189ccdbcd80a988ea73ca3fee3e9a
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-14 16:43:56 -0700

    Added VCR testing.

--- spec/service/songkick_service_spec.rb
+++ spec/service/songkick_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe SongkickService do
   it " Gets JSON data" do
-    VCR.use_cassette("Songkick_denver_casette") do
+    VCR.use_cassette("Venues_denver_casette") do
       service = SongkickService.new
       venue_data = service.get_city_venues("Denver")
 
@@ -20,18 +20,20 @@ describe SongkickService do
   end
 
   it '#get_venue_by_id' do
-    service = SongkickService.new
-    venue_data = service.get_venue_by_id(10459)
+    VCR.use_cassette("one_venue_id_casette") do
+      service = SongkickService.new
+      venue_data = service.get_venue_by_id(10459)
 
-    expect(service).to be_an_instance_of(SongkickService)
+      expect(service).to be_an_instance_of(SongkickService)
 
-    expect(venue_data).to be_a(Hash)
-    expect(venue_data).to have_key(:metroArea)
-    expect(venue_data).to have_key(:website)
-    expect(venue_data).to have_key(:city)
-    expect(venue_data).to have_key(:capacity)
-    expect(venue_data).to have_key(:phone)
-    expect(venue_data).to have_key(:lat)
-    expect(venue_data).to have_key(:lng)
+      expect(venue_data).to be_a(Hash)
+      expect(venue_data).to have_key(:metroArea)
+      expect(venue_data).to have_key(:website)
+      expect(venue_data).to have_key(:city)
+      expect(venue_data).to have_key(:capacity)
+      expect(venue_data).to have_key(:phone)
+      expect(venue_data).to have_key(:lat)
+      expect(venue_data).to have_key(:lng)
+    end
   end
 end
