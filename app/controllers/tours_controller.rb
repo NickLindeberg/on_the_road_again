cba7276594fbe71b462f3ab719229835ba13f930
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-13 19:09:47 -0700

    artist can create a tour

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,7 +1,23 @@
 class ToursController < ApplicationController
 
+  def new
+    @tour = Tour.new()
+  end
+
+  def create
+    artist = current_artist
+    tour = artist.tours.create(tour_params)
+
+    redirect_to 
+  end
+
   def show
 
   end
 
+  private
+
+  def  tour_params
+    params.require(:tour).permit(:name)
+  end
 end
---
+++ spec/features/users/user_can_create_new_tour_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Artist can create a tour' do
+  it 'names a tour' do
+    artist = create(:artist)
+    allow_any_instance_of(ApplicationController).to receive(:current_artist).and_return(artist)
+
+    visit dashboard_path
+
+    click_on "Add New Tour"
+
+    expect(current_path).to eq(new_tour_path)
+
+    fill_in "tour[name]", with: "Rad Tour"
+    click_on "Create Tour"
+    expect(current_path).to eq(dashboard_path)
+  end
+end
