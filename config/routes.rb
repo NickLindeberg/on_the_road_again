0ebac02513c10affbd8e88a81bada02cbb75df98
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-14 11:21:06 -0700

    changed routes to make sense for our controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'venues/show'
   root to: "welcome#index"
   get 'about', to: "about#index"
-  get 'dashboard', to: "artists#index"
+  get 'dashboard', to: "artists#show"
 
   resources :venues, only: [:show]
   resources :events, only: [:show, :new, :create, :edit, :update]
