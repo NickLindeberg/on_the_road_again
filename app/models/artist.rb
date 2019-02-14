5c8feaf0fea74c089ad63ab6f069d0f0f3b9af5c
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-14 13:39:12 -0700

    updates seeds and adds venue_id to events to reference the poro

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ApplicationRecord
   validates_presence_of :name, :google_token, :email, :google_client_id
   has_many :tours
+  has_many :events, through: :tours
 
   def self.find_or_create_from_auth_hash(auth)
     artist = find_or_create_by(google_client_id: auth["uid"])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_161639) do
+ActiveRecord::Schema.define(version: 2019_02_14_184519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_161639) do
     t.bigint "tour_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "venue_id"
     t.index ["tour_id"], name: "index_events_on_tour_id"
   end
 
