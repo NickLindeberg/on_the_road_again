7776cc0cce4541fda3582b5d254c7e25f01b687b
Jimmy Smith <jaystansmith@gmail.com>
2019-02-13 19:43:27 -0700

    Added forthcoming and past events to venue show page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,6 @@
 class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
+    @events = Event.all
   end
 end
--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :artist do
     name { "Mean Machine" }
-    token { "tomhanks1234" }
+    google_token { "tomhanks1234" }
     email { "whereyou@willson.com" }
     google_client_id { "google_client" }
-    refresh_token { "refresh_token" }
   end
 end
