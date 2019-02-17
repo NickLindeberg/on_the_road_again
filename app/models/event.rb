1bda8948a2107b5346112f4c49cc9666f4801928
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-16 19:25:41 -0700

    working show for event missing total revenue
    must add total revenue function

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,4 @@
 class Event < ApplicationRecord
   validates_presence_of :name, :show_time
   belongs_to :tour
-
-  def total_profit
-    total_prof = self.event_profit - self.travel_cost
-    total_prof
-  end
 end
