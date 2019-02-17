bc542c4810a69337c761038f4237a14f358e6cf5
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-17 16:13:49 -0700

    removed keys from cassettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,9 @@ VCR.configure do |config|
   config.ignore_localhost = true
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
+  config.filter_sensitive_data("<SONGKICK_API_KEY>") { ENV['SONGKICK_API_KEY'] }
+  config.filter_sensitive_data("<GOOGLE_CLIENT_ID>") { ENV['GOOGLE_CLIENT_ID'] }
+  config.filter_sensitive_data("<GOOGLE_CLIENT_SECRET>") { ENV['GOOGLE_CLIENT_SECRET'] }
 end
 
 
