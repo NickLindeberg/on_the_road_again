4113c399753286390840ffd123f8c8ff518d2d03
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-16 19:42:30 -0700

    working

--- db/migrate/20190216224655_add_event_profit_to_event.rb
+++ db/migrate/20190216224655_add_event_profit_to_event.rb
@@ -1,6 +1,6 @@
 class AddEventProfitToEvent < ActiveRecord::Migration[5.2]
   def change
-    add_column :events, :event_profit, :decimal, :scale => 2
-    add_column :events, :travel_cost, :decimal, :scale => 2
+    add_column :events, :event_profit, :decimal
+    add_column :events, :travel_cost, :decimal
   end
 end
