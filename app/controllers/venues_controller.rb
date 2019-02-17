7390ff321ace12ca1c87b817c501a37ed6ad1a39
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-17 13:04:10 -0700

    added venues controller back in per prior merge.  Should fix conflict

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,7 @@
+class VenuesController < ApplicationController
+
+  def show
+    @events = Event.where(venue_id: params[:id])
+    @venue = VenueBuilderFacade.new.find_single_venue_by_id(params[:id])
+  end
+end
