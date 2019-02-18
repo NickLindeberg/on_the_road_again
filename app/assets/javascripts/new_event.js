eb110d7ddb4e9de3eea052cabaed025cdfe53eec
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-18 13:13:21 -0700

    added logo to welcome screen, updated about page

---
+++ app/assets/javascripts/new_event.js
@@ -0,0 +1,20 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var search = document.getElementById("search");
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
