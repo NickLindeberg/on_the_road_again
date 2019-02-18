5e83daaa4a10eb178496bf0f2f10c3210188efcb
Jimmy Smith <jaystansmith@gmail.com>
2019-02-17 20:16:23 -0700

    Added tour stats functionality

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,11 +10,13 @@ require 'vcr'
 require 'webmock/rspec'
 
 VCR.configure do |config|
-  config.ignore_localhost = true
-  config.cassette_library_dir = 'spec/cassettes'
-  config.hook_into :webmock
+ config.ignore_localhost = true
+ config.cassette_library_dir = 'spec/cassettes'
+ config.hook_into :webmock
+ config.filter_sensitive_data("<SONGKICK_API_KEY>") { ENV['SONGKICK_API_KEY'] }
+ config.filter_sensitive_data("<GOOGLE_CLIENT_ID>") { ENV['GOOGLE_CLIENT_ID'] }
+ config.filter_sensitive_data("<GOOGLE_CLIENT_SECRET>") { ENV['GOOGLE_CLIENT_SECRET'] }
 end
-
 
 
 # Add additional requires below this line. Rails is not loaded until this point!
