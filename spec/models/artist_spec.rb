3e9b51af53c0231ac2807b9a46a263d88e75ec59
Nick Lindeberg <lindebergnick@gmail.com>
2021-01-13 15:40:03 -0700

    massive spec refactors

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -12,36 +12,23 @@ RSpec.describe Artist, type: :model do
     it { should have_many(:tours) }
   end
 
-  let(:artist) { create(:artist) }
-  let(:tour)   { create(:tour, artist_id: artist.id) }
-
   describe "Methods" do
+    let(:artist)        { create(:artist) }
+    let(:past_tour)     { create(:tour, artist_id: artist.id) }
+    let(:future_tour)   { create(:tour, artist_id: artist.id) }
+    let!(:future_event) { create(:event, tour_id: future_tour.id, show_date: 10.days.from_now) }
+    let!(:past_event)   { create(:event, tour_id: past_tour.id, show_date: 10.days.ago) }
+
     it "#future_tours" do
-      event_1 = create(:event, tour_id: tour.id, show_date: 10.days.from_now )
-      event_2 = create(:event, tour_id: tour.id, show_date: 11.days.from_now )
-      event_3 = create(:event, tour_id: tour.id, show_date: 12.days.from_now )
-
-      tour_2 = create(:tour, artist_id: artist.id)
-      event_1 = create(:event, tour_id: tour_2.id, show_date: 10.days.ago )
-      event_2 = create(:event, tour_id: tour_2.id, show_date: 11.days.ago )
-      event_3 = create(:event, tour_id: tour_2.id, show_date: 12.days.ago )
-
       expect(artist.future_tours).to be_a(Array)
-      expect(artist.future_tours).to eq([tour])
+      expect(artist.future_tours).to eq([future_tour])
+      expect(artist.future_tours).not_to eq([past_tour])
     end
 
     it "#past_tours" do
-      event_1 = create(:event, tour_id: tour.id, show_date: 10.days.from_now )
-      event_2 = create(:event, tour_id: tour.id, show_date: 11.days.from_now )
-      event_3 = create(:event, tour_id: tour.id, show_date: 12.days.from_now )
-
-      tour_2 = create(:tour, artist_id: artist.id)
-      event_1 = create(:event, tour_id: tour_2.id, show_date: 10.days.ago )
-      event_2 = create(:event, tour_id: tour_2.id, show_date: 11.days.ago )
-      event_3 = create(:event, tour_id: tour_2.id, show_date: 12.days.ago )
-
       expect(artist.past_tours).to be_a(Array)
-      expect(artist.past_tours).to eq([tour_2])
+      expect(artist.past_tours).to eq([past_tour])
+      expect(artist.past_tours).not_to eq([future_tour])
     end
   end
 end
--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -11,78 +11,47 @@ RSpec.describe Tour, type: :model do
   end
 
   describe "Methods" do
-    it "#future_events_list" do
-      artist_1 = create(:artist)
-      tour = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, tour_id: tour.id, show_date: "2017-02-11 21:10:51" )
-      event_2 = create(:event, tour_id: tour.id, show_date: "2020-02-11 21:10:51" )
-      event_3 = create(:event, tour_id: tour.id, show_date: "2021-02-11 21:10:51" )
+    let(:artist)      { create(:artist) }
+    let(:tour)        { create(:tour, artist_id: artist.id) }
+    let(:tour_2)      { create(:tour, artist_id: artist.id) }
+    let!(:event_1)    { create(:event, tour: tour, show_date: 12.days.ago) }
+    let!(:event_2)    { create(:event, tour: tour, show_date: future_show) }
+    let(:future_show) { 10.days.from_now }
 
-      expect(tour.future_events_list).to be_a(Array)
-      expect(tour.future_events_list).to eq([event_2, event_3])
+    context 'event lists' do
+      it "#past_events_list" do
+        expect(tour.past_events_list).to be_a(Array)
+        expect(tour.past_events_list).to eq([event_1])
+      end
+
+      it "#future_events_list" do
+        expect(tour.future_events_list).to be_a(Array)
+        expect(tour.future_events_list).to eq([event_2])
+      end
     end
 
-    it "#past_events_list" do
-      artist_1 = create(:artist)
-      tour = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, tour_id: tour.id, show_date: "2017-02-11 21:10:51" )
-      event_2 = create(:event, tour_id: tour.id, show_date: "2018-02-11 21:10:51" )
-      event_3 = create(:event, tour_id: tour.id, show_date: "2021-02-11 21:10:51" )
+    context "past and future events" do
+      let!(:event_2)  { create(:event, tour: tour_2, show_date: future_show) }
 
-      expect(tour.past_events_list).to be_a(Array)
-      expect(tour.past_events_list).to eq([event_1, event_2])
+      it "#past_events?" do
+        expect(tour.past_events?).to eq(true)
+        expect(tour_2.past_events?).to eq(false)
+      end
+
+      it "#future_events?" do
+        expect(tour.future_events?).to eq(false)
+        expect(tour_2.future_events?).to eq(true)
+      end
     end
 
-    it "#past_events?" do
-      artist_1 = create(:artist)
-      tour = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, tour_id: tour.id, show_date: "2017-02-11 21:10:51" )
-      event_2 = create(:event, tour_id: tour.id, show_date: "2018-02-11 21:10:51" )
-      event_3 = create(:event, tour_id: tour.id, show_date: "2015-02-11 21:10:51" )
+    context "start and end dates" do
+      it "#tour_start_date" do
+        expect(tour.start_date).to eq(event_1.show_date)
+      end
 
-      tour_2 = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, tour_id: tour_2.id, show_date: "2022-02-11 21:10:51" )
-      event_2 = create(:event, tour_id: tour_2.id, show_date: "2023-02-11 21:10:51" )
-      event_3 = create(:event, tour_id: tour_2.id, show_date: "2025-02-11 21:10:51" )
-
-      expect(tour.past_events?).to eq(true)
-      expect(tour_2.past_events?).to eq(false)
-    end
-
-    it "#future_events?" do
-      artist_1 = create(:artist)
-      tour = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, tour_id: tour.id, show_date: "2017-02-11 21:10:51" )
-      event_2 = create(:event, tour_id: tour.id, show_date: "2018-02-11 21:10:51" )
-      event_3 = create(:event, tour_id: tour.id, show_date: "2015-02-11 21:10:51" )
-
-      tour_2 = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, tour_id: tour_2.id, show_date: "2022-02-11 21:10:51" )
-      event_2 = create(:event, tour_id: tour_2.id, show_date: "2023-02-11 21:10:51" )
-      event_3 = create(:event, tour_id: tour_2.id, show_date: "2025-02-11 21:10:51" )
-
-      expect(tour.future_events?).to eq(false)
-      expect(tour_2.future_events?).to eq(true)
-    end
-
-    it "#tour_start_date" do
-      artist_1 = create(:artist)
-      tour = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, name: "first", tour_id: tour.id, show_date: "2017-01-11 21:10:51" )
-      event_2 = create(:event, name: "second", tour_id: tour.id, show_date: "2017-02-11 21:10:51" )
-      event_3 = create(:event, name: "third", tour_id: tour.id, show_date: "2017-03-11 21:10:51" )
-
-      expect(tour.start_date).to eq(event_1.show_date)
-    end
-
-    it "#tour_end_date" do
-      artist_1 = create(:artist)
-      tour = create(:tour, artist_id: artist_1.id)
-      event_1 = create(:event, name: "first", tour_id: tour.id, show_date: "2017-01-11 21:10:51" )
-      event_2 = create(:event, name: "second", tour_id: tour.id, show_date: "2017-02-11 21:10:51" )
-      event_3 = create(:event, name: "third", tour_id: tour.id, show_date: "2017-03-11 21:10:51" )
-
-      expect(tour.end_date).to eq(event_3.show_date)
+      it "#tour_end_date" do
+        expect(tour.end_date).to eq(event_2.show_date)
+      end
     end
   end
 end
