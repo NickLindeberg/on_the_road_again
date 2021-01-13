7a38afdc8509f743a4c532f1a7a5fc243dbb3e0c
Nick Lindeberg <lindebergnick@gmail.com>
2021-01-13 15:56:43 -0700

    update events spec

--- spec/features/venues/user_can_see_past_or_present_events_spec.rb
+++ spec/features/venues/user_can_see_past_or_present_events_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "AS a user" do
   describe "When I visit the venue show page" do
+    let(:artist) { create(:artist)}
+    let(:tour)   { create(:tour, artist: artist)}
+
     it "I can see forthcoming" do
       VCR.use_cassette("Venues_denver_forthcoming_events_casette") do
-        artist = create(:artist)
-        tour = create(:tour, artist: artist)
         event = create(:event, tour: tour, show_date: 10.days.from_now)
-        event2 = create(:event, name: "Wily Coyote!", tour: tour, show_date: 10.days.ago)
+        event2 = create(:event, name: "Wily Coyote!", tour: tour, show_date:"2015-10-01")
 
         visit venue_path(event.venue_id)
         within(".f-tours") do
@@ -16,19 +17,17 @@ RSpec.feature "AS a user" do
         end
       end
     end
-  end
 
     it "I can see past events" do
       VCR.use_cassette("Venues_denver_past_events_casette") do
-      artist = create(:artist)
-      tour = create(:tour, artist: artist)
-      event = create(:event, name: "On The Rocks", tour: tour, show_date: 10.days.ago)
-      event2 = create(:event, tour: tour, show_date: 10.days.from_now)
+        event = create(:event, name: "On The Rocks", tour: tour, show_date:"2017-09-01")
+        event2 = create(:event, tour: tour, show_date: 10.days.from_now)
 
-      visit venue_path(event.venue_id)
-      within(".p-tours") do
-        expect(page).to have_content(event.name)
-        expect(page).not_to have_content(event2.name)
+        visit venue_path(event.venue_id)
+        within(".p-tours") do
+          expect(page).to have_content(event.name)
+          expect(page).not_to have_content(event2.name)
+        end
       end
     end
   end
