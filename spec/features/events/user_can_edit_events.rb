5ba0e05bbce1affec9e96013d606fb84e5f3858c
Nick Lindeberg <lindebergnick@gmail.com>
2021-01-13 16:37:27 -0700

    update user can edit event spect

--- spec/features/events/user_can_edit_events.rb
+++ spec/features/events/user_can_edit_events.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe 'User can edit an event' do
+  let(:artist) { create(:artist) }
+
   it 'is redirected to event show page' do
     VCR.use_cassette("record-edit") do
-      artist = create(:artist)
       tour = create(:tour, artist:artist)
       event = create(:event, tour: tour, name: "Renegade", show_date: "2018-02-02", show_time: "8:00 PM")
       allow_any_instance_of(ApplicationController).to receive(:current_artist).and_return(artist)
@@ -13,7 +14,7 @@ describe 'User can edit an event' do
       fill_in "show_date",	with: "2018-08-02"
       fill_in "show_time",	with: "10:00 PM"
       click_on "Update Event"
-      
+
       expect(current_path).to eq(event_path(event))
       expect(event.name).to_not eq("Renegade")
       expect(event.name).to_not eq("2018-02-02")
